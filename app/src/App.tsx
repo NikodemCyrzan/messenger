3017ae9d125e8f772aff35ce8ced9ca8ba25e15d
Nikodem Cyrzan <85363474+NikodemCyrzan@users.noreply.github.com>
2023-06-29 10:51:34 +0200

    style: removed commented code

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,6 @@
 import WebsocketClient from "./utils/websocketClient";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import ChatView from "./views/ChatView/ChatView";
-// import LoginView from "./views/LoginView/LoginView";
 import RegisterView from "./views/RegisterView/RegisterView";
 import Memory from "./utils/memory";
 import { useEffect } from "react";
@@ -12,30 +10,6 @@ import LoginView from "./views/LoginView/LoginView";
 function App() {
     useEffect(() => {
         WebsocketClient.start("ws://localhost", 20000);
-
-        //     WebsocketClient.addOnReady(() => {
-        //         const username = Math.random().toString(36).substring(7);
-        //         WebsocketClient.sendRequest(
-        //             "REGISTER",
-        //             {
-        //                 username: username,
-        //                 password: "test",
-        //             },
-        //             () => {
-        //                 console.log(Memory.UserId);
-        //                 WebsocketClient.sendRequest(
-        //                     "LOGIN",
-        //                     {
-        //                         username: username,
-        //                         password: "test",
-        //                     },
-        //                     (response) => {
-        //                         console.log(response);
-        //                     }
-        //                 );
-        //             }
-        //         );
-        //     });
     }, []);
 
     return (
